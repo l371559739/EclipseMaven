41a3eecfd403d5653417eaaac5fad54c3fd1d4de
lrqzj <lrqzj>
2013-09-23 23:27:12 +0800

    eclipse maven

---
+++ EclipseMaven/src/main/java/lrq/maven/EclipseMaven/App.java
@@ -0,0 +1,13 @@
+package lrq.maven.EclipseMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
